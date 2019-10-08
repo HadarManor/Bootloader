3687bb681ffd3f3910bee52103de855779923258
Hadar <hadarmanoron@gmail.com>
2019-10-08 18:37:16 +0300

    added a print to the boot sector

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,8 +1,20 @@
-; Inf loop
-loop:
-	jmp loop
+
+mov ah, 0x0e ;tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+	
+	
+jmp $ ; jump to current address - inf loop
+
 
 ; filling 510 zeros minus the size of the prev code^
 times 510-($-$$) db 0
-
+;magic number for the boot sector
 dw 0xaa55
